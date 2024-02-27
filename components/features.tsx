96fd4243746f1751772c47c8950363aa39f4b2b2
Ivan-Mandujano <ivanmandujano594@gmail.com>
2024-02-27 16:19:45 -0600

    Editado el header

--- components/features.tsx
+++ components/features.tsx
@@ -5,6 +5,7 @@ import { Transition } from '@headlessui/react'
 import Image from 'next/image'
 import FeaturesBg from '@/public/images/features-bg.png'
 import FeaturesElement from '@/public/images/features-element.png'
+import Peces from '@/components/Peces';
 
 export default function Features() {
   
@@ -15,7 +16,12 @@ export default function Features() {
   const heightFix = () => {
     if (tabs.current && tabs.current.parentElement) tabs.current.parentElement.style.height = `${tabs.current.clientHeight}px`
   }
-
+  const objetos = [
+    { imagen: 'Image', titulo: 'Título 1' },
+    { imagen: 'Image', titulo: 'Título 2' },
+    { imagen: 'Image', titulo: 'Título 3' },
+  ];
+  
   useEffect(() => {
     heightFix()
   }, []) 
@@ -24,17 +30,17 @@ export default function Features() {
     <section className="relative">
 
       {/* Section background (needs .relative class on parent and next sibling elements) */}
-      <div className="absolute inset-0 bg-gray-100 pointer-events-none mb-16" aria-hidden="true"></div>
-      <div className="absolute left-0 right-0 m-auto w-px p-px h-20 bg-gray-200 transform -translate-y-1/2"></div>
 
       <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="pt-12 md:pt-20">
-
+        <div className="">
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="h2 mb-4">Explore the solutions</h1>
-            <p className="text-xl text-gray-600">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat.</p>
+            <h1 className="h2 mb-4">Los peces que tenemos</h1>
+            <p className="text-xl text-gray-600">Contamos con una gran variedad de peces criados en condiciones optimas</p>
+            <Peces />
           </div>
+          
+
 
           {/* Section content */}
           <div className="md:grid md:grid-cols-12 md:gap-6">
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,5 +1,7 @@
-import VideoThumb from '@/public/images/hero-image.png'
+import VideoThumb from '#/hero-image.png'
 import ModalVideo from '@/components/modal-video'
+import Banner from "#/banner.png"
+import Image from 'next/image'
 
 export default function Hero() {
   return (
@@ -22,40 +24,24 @@ export default function Hero() {
         </svg>
       </div>
 
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+      <div className="relative h-screen overflow-hidden">
 
-        {/* Hero content */}
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+{/* Hero content */}
+<div className="absolute inset-0 flex items-center justify-center">
 
-          {/* Section header */}
-          <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Make your website <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">wonderful</span></h1>
-            <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
-              <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
-                <div>
-                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
-                </div>
-                <div>
-                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
-                </div>
-              </div>
-            </div>
-          </div>
+  {/* Hero image */}
+  <img src={Banner.src} alt="banner" className="w-full h-60 md:h-full object-cover" />
 
-          {/* Hero image */}
-          <ModalVideo
-            thumb={VideoThumb}
-            thumbWidth={768}
-            thumbHeight={432}
-            thumbAlt="Modal video thumbnail"
-            video="/videos/video.mp4"
-            videoWidth={1920}
-            videoHeight={1080} />
+  {/* Texto centrado */}
+  <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
+    <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Acuario <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">Keolmar</span></h1>
+    <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150"></p>
+    <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300"></div>
+  </div>
 
-        </div>
+</div>
 
-      </div>
+</div>
     </section>
   )
 }
